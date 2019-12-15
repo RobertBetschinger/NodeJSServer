498e1ee2d842f3731c87d574550d485b88e1d118
Robert Betschinger <robert-betschinger@outlook.de>
2019-12-15 23:02:55 +0100

    Added Headers for Question Route

--- routes/questions.js
+++ routes/questions.js
@@ -5,6 +5,7 @@ const Question = require('../models/question')
 router.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
   next();
 });
 
